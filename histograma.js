00c8475dbe3f65c8a54b289559090944c7fc9f5f
luisdelgado <lhds@cin.ufpe.br>
2017-06-13 20:22:54 -0300

    Update scales in histogram

--- histograma.js
+++ histograma.js
@@ -3,13 +3,7 @@ function histogram (array) {
 	// Setando dimensoes e margens do SVG
 	var margin = {top: 3, right: 2, bottom: 5, left: 330};
 	var width = 160 - margin.left - margin.right;
-	var height = 180 - margin.top - margin.bottom;	
-
-	// Criando escalas
-	var xScale = d3.scaleLinear().domain([0,100]).range([0,width]);
-	var yScale = d3.scaleLinear().domain([0,100]).range([height,0]);
-	var zScale = d3.scaleLinear().domain([1000,0]).range([height,0]);
-	var cScale = d3.scaleLinear().domain([1000,0]).range([0,width]);	
+	var height = 180 - margin.top - margin.bottom;		
 
 	// Criando SVG
 	var myUpdate = d3.select("body")
@@ -24,7 +18,7 @@ function histogram (array) {
 			.append("g")
 			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-	//Contando número c/s vítimas
+	// Contando número c/s vítimas
 	var semVitima = 0;
 	var comVitima = 0;	
 	for (var i=0; i < array.length ; ++i) {
@@ -38,13 +32,18 @@ function histogram (array) {
 	console.log(comVitima);
 	console.log(semVitima);
 
+	// Criando escalas
+	var xScale = d3.scaleLinear().domain([0,100]).range([0,130]);
+	var yScale = d3.scaleLinear().domain([0,semVitima + comVitima]).range([height,0]);
+	var histogramScale = d3.scaleLinear().domain([semVitima + comVitima,0]).range([height,0]);
+
 	// Filtrando apenas dados necessários
 	var vitimas = [{nome: "Sem Vítima", vitimas: semVitima}, {nome: "Com Vítima", vitimas: comVitima}];
 
 	// Criando eixos
 	var xAxisGroup = mySVG.append("g")
 			.attr("class","xAxis")
-			.attr("transform","translate(31,"+(height-margin.top+21)+") scale(-1,1)");
+			.attr("transform","translate(30,"+(height-margin.top+21)+")");
 	var xAxis = d3.axisBottom(xScale)
 			.ticks(0);
 	xAxisGroup.call(xAxis);
@@ -56,19 +55,21 @@ function histogram (array) {
 	yAxisGroup.call(yAxis);
 
 	// Criando histograma
-	var histogram = d3.histogram()
-	    .domain(xScale.domain())
-	    .thresholds(xAxis.ticks(20));
-
 	mySVG
 		.selectAll("rect")
 	    .data(vitimas)
 	    .enter().append("rect")
 	    .attr("transform","translate(30," + (height-margin.top+21)+ ") scale(1,-1)")
-	    .attr("x", function(d){return zScale(d.vitimas/10);})
+	    .attr("x", function(d){
+	    	if (d.nome=="Sem Vítima") {
+        		return 10;
+      		} else {
+        		return 69;
+      		}
+	    })
 	    .attr("y", 0)
 	    .attr("width", 50)
-	    .attr("height", function(d) {return zScale(d.vitimas/10);})
+	    .attr("height", function(d) {return histogramScale(d.vitimas);})
 	    .attr("fill", function(d) {
       		if (d.nome=="Sem Vítima") {
         		return "blue";
@@ -77,17 +78,24 @@ function histogram (array) {
       		}
     	});
 
-	// Colocando compainhas 
+	// Colocando textos 
 	var text = mySVG.selectAll("circle")
 		.data(vitimas)
 		.enter()
 		.append("text");
 
 	var labelTexts = text
-		.attr("x", function(d) {return zScale((d.vitimas+1400)/10); })
+		.attr("transform","translate(30,0)")
+		.attr("x", function(d) {
+			if (d.nome=="Sem Vítima") {
+        		return 10;
+      		} else {
+        		return 69;
+      		}
+	    })
 	    .attr("y", function(d) {return height + margin.top + margin.bottom + 20;})
 	    .text( function (d) { return d.nome; })
 	    .attr("font-family", "sans-serif")
-		.attr("font-size", "10px")
+		.style("font-size", "9px")
 	    .style("fill", "black");
 }
