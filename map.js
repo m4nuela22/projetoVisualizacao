7c2531ca901a999a3dded49182036725aa80fa9d
m4nuela <manu_ela10@hotmail.com>
2017-06-15 13:18:03 -0300

    using SVG to plot circles instead of leaflet markers

--- map.js
+++ map.js
@@ -3,32 +3,13 @@ var mapSVG;
 var transform;
 var path;
 var featureElement;
+var points;
+var currentZoom = 12;
+var cScale = d3.scaleOrdinal().domain(["COM VÍTIMA","SEM VÍTIMA"]).range(["red","blue"]);
 
-// var greenIcon = L.icon({
-//     iconUrl: 'green.jpg',
-//     shadowUrl: 'green.jpg',
-//
-//     iconSize:     [6, 6], // size of the icon
-//     shadowSize:   [6, 6], // size of the shadow
-//     iconAnchor:   [6, 6], // point of the icon which will correspond to marker's location
-//     shadowAnchor: [6, 6],  // the same for the shadow
-//     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-// });
-//
-//
-// var redIcon = L.icon({
-//     iconUrl: 'red.jpg',
-//     shadowUrl: 'red.jpg',
-//
-// 		iconSize:     [6, 6], // size of the icon
-//     shadowSize:   [6, 6], // size of the shadow
-//     iconAnchor:   [6, 6], // point of the icon which will correspond to marker's location
-//     shadowAnchor: [6, 6],  // the same for the shadow
-//     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-// });
 
 function createMap(){
-	mymap = L.map('mapDiv').setView([-8.056223, -34.913620], 12);
+	mymap = L.map('mapDiv').setView([-8.056223, -34.913620], currentZoom);
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 		maxZoom: 18,
 		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
@@ -36,20 +17,11 @@ function createMap(){
 			'Imagery © <a href="http://mapbox.com">Mapbox</a>',
 		id: 'mapbox.streets'
 	}).addTo(mymap);
+
   L.svg().addTo(mymap);
   mapSVG = d3.select("#mapDiv").select("svg");
-  // var district_boundary = new L.geoJson();
-  // district_boundary.addTo(mymap);
-  //
-  // $.ajax({
-  // dataType: "json",
-  // url: "bairros.geojson",
-  // success: function(data) {
-  //     $(data.features).each(function(key, data) {
-  //         district_boundary.addData(data);
-  //     });
-  // }
-  // });
+
+
   d3.json("bairros.geojson",function(error, geojson) {
     if (error){
       throw error;
@@ -57,88 +29,54 @@ function createMap(){
     transform = d3.geoTransform({point: projectPoint});
   	path = d3.geoPath().projection(transform);
 
-    featureElement = mapSVG.selectAll("path")
-              .data(geojson.features)
-              .enter()
-              .append("path")
-              .attr("stroke", "blue")
-              .attr("fill", "white")
-              .attr("fill-opacity", 0)
+    L.geoJson(geojson, { weight: 1 }).addTo(mymap);
 
     mymap.on("zoom", update);
 		update();
 
     function update() {
     	currentZoom = mymap.getZoom();
-      featureElement.attr("d", path);
+      //featureElement.attr("d", path);
+      if (points){
+        points.attr("transform",
+        function(d) {
+            var point = getPoint(d.latitude,d.longitude);
+            return "translate("+
+            point.x +","+
+            point.y +")";
+  			});
+      }
     }
 
   });
-
+}
 
 function addMarkersToMap(array){
 	var lat;
 	var long;
-	var tipo;
 
-  mapSVG.selectAll("circle")
+	points = mapSVG.selectAll("circle").remove();
+
+  points = mapSVG.selectAll("circle")
   .data(array)
   .enter()
   .append("circle")
-  .attr("x",projectPoint())
-  .attr("y",)
-  .attr("r",)
-  .attr("fill",)
+  .attr("r",2)
+  .attr("transform", function(d) {
+    var point = getPoint(d.latitude,d.longitude);
+    return "translate("+
+    point.x +","+
+    point.y +")";})
+  .attr("fill",function(d){return cScale(d.tipo);});
+}
 
-	// for (var i = 0 ; i< array.length; i++){
-	// 	lat = array[i].latitude;
-	// 	long = array[i].longitude;
-	// 	tipo = array[i].tipo;
-	// 	if (tipo === "COM VÍTIMA"){
-	// 		L.marker([lat,long],{icon: redIcon}).addTo(mymap).bindPopup("I am a red leaf.");
-	// 	}else if (tipo === "SEM VÍTIMA"){
-	// 		L.marker([lat,long],{icon: greenIcon}).addTo(mymap).bindPopup("I am a green leaf.");
-	// 	}else{
-	// 		console.log("error");
-	// 	}
-	// }
+function getPoint(x,y){
+  var latLong = new L.LatLng(x, y);
+  var point = mymap.latLngToLayerPoint(latLong);
+  return point;
 }
 
 function projectPoint(x, y) {
        var point = mymap.latLngToLayerPoint(new L.LatLng(y, x));
        this.stream.point(point.x, point.y);
 }
-
-// function update() {
-// 			currentZoom = mymap.getZoom();
-//       featureElement.attr("d", path);
-			// reposotion dots
-			// feature.attr("transform",
-			// 	function(d) {
-			// 		return "translate("+
-			// 			mymap.latLngToLayerPoint(d.LatLng).x +","+
-			// 			mymap.latLngToLayerPoint(d.LatLng).y +")";
-			// })
-
-
-
-			// if(currentColoring == 1){
-			// 	feature.style('fill', function(d){ return getCapColor(d); })
-			// 	feature.style('stroke', function(d){  return getCapColor(d); })
-			// } else {
-			// 	feature.style('fill', function(d){ return getColor(d, ids); })
-			// 	feature.style('stroke', function(d){  return getColor(d, ids); })
-			// }
-
-			// ToDo: show fewer dots when zoomed out
-			// // update syle of dots, according to zoom level
-			// if(currentZoom < 12){
-			// 			feature.style('opacity', function(d){ if(d['CAP_COLOR'] > 5){return 1;}
-			// 													else{ return .3}} )
-			// 			feature.attr('r', function(d){ if(d['CAP_COLOR'] > 5){return 2;}
-			// 													else{ return 1}} )
-			// } else {
-			// 	feature.style('opacity', .6);
-			// }
-
-		}
