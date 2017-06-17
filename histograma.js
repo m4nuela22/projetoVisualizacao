8bbf4a800d2c7c88ba01d0988050e6ea5ad93514
luisdelgado <lhds@cin.ufpe.br>
2017-06-17 13:10:46 -0300

    Return data selected from click on histogram

--- histograma.js
+++ histograma.js
@@ -1,5 +1,12 @@
 var mainSVG = d3.selectAll("#mainSVG");
 
+var semVitima = 0;
+var comVitima = 0;
+var vitimasArray = [{id: "SemVítima", nome: "Sem Vítima", value: "SEM VÍTIMA", vitimas: semVitima, selected: false}, {id: "ComVítima", nome: "Com Vítima", value: "COM VÍTIMA", vitimas: comVitima, selected: false}];
+var csv;
+var numberOfSelections = 0;
+var myDispath = d3.dispatch("selectionChanged");
+
 function histogram (array) {
 
 	// Setando dimensoes e margens do SVG
@@ -8,10 +15,13 @@ function histogram (array) {
 	var height = 180 - margin.top - margin.bottom;
 
 	clearEverythingHist();
+	csv = array;
+	semVitima = 0;
+	comVitima = 0;
+	vitimasArray = [{id: "SemVítima", nome: "Sem Vítima", value: "SEM VÍTIMA", vitimas: semVitima, selected: false}, {id: "ComVítima", nome: "Com Vítima", value: "COM VÍTIMA", vitimas: comVitima, selected: false}];
+	numberOfSelections = 0;
 
 	// Contando número c/s vítimas
-	var semVitima = 0;
-	var comVitima = 0;
 	for (var i=0; i < array.length ; ++i) {
 		tipo = array[i].tipo;
 	  	if (tipo=="COM VÍTIMA") {
@@ -26,12 +36,13 @@ function histogram (array) {
 	var yScale = d3.scaleLinear().domain([0,semVitima + comVitima]).range([height,0]);
 	var histogramScale = d3.scaleLinear().domain([semVitima + comVitima,0]).range([height,0]);
 
-	// Filtrando apenas dados necessários
-	var vitimas = [{nome: "SemVítima", vitimas: semVitima, selected: false}, {nome: "ComVítima", vitimas: comVitima, selected: false}];
-
 	var padding = 90;
 	var margemEsquerdaMin = 659;
 	var margemEsquerdaMax = 728;
+
+	// Atualizando array
+	vitimasArray[0].vitimas = semVitima;
+	vitimasArray[1].vitimas = comVitima;
 
 	// Criando eixos
 	var xAxisGroup = mainSVG.append("g")
@@ -50,14 +61,14 @@ function histogram (array) {
 	// Criando histograma
 	mainSVG
 		.selectAll()
-	    .data(vitimas)
+	    .data(vitimasArray)
 	    .enter().append("rect")
 		.attr("class","rectHist")
 		.attr("class","pointer")
-		.attr("id", function (d) { return d.nome; })
+		.attr("id", function (d) { return d.id; })
 	    .attr("transform","translate(" + padding + "," + (height-margin.top+21)+ ") scale(1,-1)")
 	    .attr("x", function(d){
-	    	if (d.nome=="SemVítima") {
+	    	if (d.nome=="Sem Vítima") {
         		return 669;
       		} else {
         		return 728;
@@ -67,7 +78,7 @@ function histogram (array) {
 	    .attr("width", 50)
 	    .attr("height", function(d) {return histogramScale(d.vitimas);})
 	    .attr("fill", function(d) {
-      		if (d.nome=="SemVítima") {
+      		if (d.nome=="Sem Vítima") {
         		return "blue";
       		} else {
         		return "red";
@@ -77,18 +88,18 @@ function histogram (array) {
 
 	// Colocando textos
 	var text = mainSVG.selectAll()
-		.data(vitimas)
+		.data(vitimasArray)
 		.enter()
 		.append("text")
 		.attr("class","textHist");
 
 	var labelTexts = text
-		.attr("transform","translate(0,0)")
+		.attr("transform","translate(90,0)")
 		.attr("x", function(d) {
-			if (d.nome=="SemVítima") {
-        		return 669+93;
+			if (d.nome=="Sem Vítima") {
+        		return 669;
       		} else {
-        		return 728+93;
+        		return 728;
       		}
 	    })
 	    .attr("y", function(d) {return height + margin.top + margin.bottom + 20;})
@@ -99,7 +110,7 @@ function histogram (array) {
 
 	// Colocando porcentagem
 	var percentage = mainSVG.selectAll()
-		.data(vitimas)
+		.data(vitimasArray)
 		.enter()
 		.append("text")
 		.attr("class","percHist");
@@ -107,7 +118,7 @@ function histogram (array) {
 	var labelPercentage = percentage
 		.attr("transform","translate(103,0)")
 		.attr("x", function(d) {
-			if (d.nome=="SemVítima") {
+			if (d.nome=="Sem Vítima") {
         		return 669;
       		} else {
         		return 728;
@@ -128,11 +139,13 @@ function clearEverythingHist(){
 }
 
 function histogramSelected(d) {
+
+	// Mudando cor dos selecionados
 	if(d.selected == true){
     	d.selected = false;
-    	d3.select("#" + d.nome)
+    	d3.select("#" + d.id)
     	.attr("fill", function(d) {
-      		if (d.nome=="SemVítima") {
+      		if (d.nome=="Sem Vítima") {
         		return "blue";
       		} else {
         		return "red";
@@ -141,8 +154,47 @@ function histogramSelected(d) {
     	numberOfSelections -= 1;
   	}else{
     	d.selected = true;
-    	d3.select("#" + d.nome)
+    	d3.select("#" + d.id)
     	.attr("fill","#A9A9A9");
     	numberOfSelections += 1;
   	}
+
+  	// Filtrando database de acordo com seleção
+  	var result = searchOnArray();
+
+  	// Avisando aos demais componentes as mudanças
+  	//myDispath.call("selectionChanged",{who:"map",selectedList:result});
+}
+
+function searchOnArray(d) {
+
+	// Verificando quem está selecionado
+	var vitimaFiltered = vitimasArray.filter(function(d){return d.selected == true;}).map(function(d){return d.value;});
+
+	// Filtrando
+	var results  = [];
+	if (numberOfSelections == 0 || numberOfSelections == 2){
+    	results = csv;
+  	} else {
+       results = results.concat(findRowsByVitima(csv,vitimaFiltered));
+ 	}
+	return results;
+}
+
+function findRowsByVitima(database,vitimaFiltered){
+
+	// Percorrendo database na coluna Tipo
+  	var relevantArray = []
+  	for (var i = 0 ; i<database.length; i++){
+      	var row = database[i];
+      	var tipo = row.tipo;
+
+    	if (tipo != null && tipo != undefined) {
+    		// Verificando se essa linha é relevante
+        	if (tipo == vitimaFiltered[0]) {
+        		relevantArray.push(row);
+        	}
+        }
+ 	}
+  	return relevantArray;
 }
